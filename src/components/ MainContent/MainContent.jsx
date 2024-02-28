cc757aac6b17fe5bf0705492d54bc6de7dcbe49d
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-02-28 15:57:32 +0000

    play with layout

--- src/components/ MainContent/MainContent.jsx
+++ src/components/ MainContent/MainContent.jsx
@@ -8,17 +8,22 @@ export default function MainContent() {
   return (
     <div className={styles.app}>
       <header className={styles.header}>
-        <h1 className={styles.title}>Hello! 👋 I'm Elzbieta, call Ella.</h1>
-        <img src={LogoMe} alt="My logo" className={styles.logo} />
-        <h2 className={styles.subtitle}>Web Developer & Product Owner</h2>
+        <h1 className={styles.title}>
+          {/* Hello! 👋  */}
+          I'm Elzbieta, call Ella.</h1>
       </header>
+
+      <div className={styles.logo}>
+        <img src={LogoMe} alt="Elzbieta Szymichowska" className={styles.heroImage}/>
+      </div>
       
       <div className={styles.mainContent}>
         <section className={styles.intro}>
+        <h2 className={styles.subtitle}>Web Developer & Product Owner</h2>
           <p className={styles.description}>With a passion for web development and a global perspective, I'm ready to embark on my journey in the world of coding. Having lived and studied in diverse countries like Malta and Georgia, I bring a unique blend of creativity and technical expertise to every project.</p>
         </section>
 
-        <section className={styles.skills}>
+        {/* <section className={styles.skills}>
           <h2 className={styles.heading}>Skills & Toolbox</h2>
           <p>Crafting web and mobile wonders with a palette of Figma, HTML5, CSS3, JavaScript, React, and a dash of boundless curiosity. I’m always on the move, adding more tools to my kit.</p>
         </section>
@@ -38,7 +43,7 @@ export default function MainContent() {
             <a href="https://www.linkedin.com/in/elzbieta-szymichowska-31309b289/" target='_blank'><FaLinkedin/></a>
             <a href='mailTo:ela.buu@gmail.com' target='_blank'><GrMail/></a>
           </div>
-        </section>
+        </section> */}
       </div>
 
       <footer className={styles.footer}>
